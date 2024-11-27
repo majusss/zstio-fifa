56d3e9097d7650c129bfd50146064f744569538e
Kacper Majcher <majchermajus@proton.me>
2024-11-27 20:35:24 +0100

    style: fix mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={cx(sfPro.variable, inter.variable)}>
-        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-red-200" />
-        <main className="flex min-h-screen w-full flex-col items-center justify-center">
+        <div className="fixed h-[100lvh] w-full bg-gradient-to-br from-indigo-50 via-white to-red-200" />
+        <main className="flex min-h-screen w-full flex-col items-center justify-center overflow-x-hidden">
           {children}
         </main>
       </body>
