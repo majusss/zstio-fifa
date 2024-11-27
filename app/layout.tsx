aca6b69b80256fbd6301759202da9646f9becdbb
Kacper Majcher <majchermajus@proton.me>
2024-11-27 12:01:42 +0000

    aa

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+
+export const metadata = {
+  title: "FIFA 23 - ZSTiO Jarosław",
+  description: "",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-red-200" />
+        <main className="flex min-h-screen w-full flex-col items-center justify-center">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
